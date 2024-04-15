1349d00b15a2fbcfa82c8710e1df8ae27b7c92cd
looper12349 <amriteshindal29@gmail.com>
2024-04-15 12:56:16 +0530

    Installed Jpa Library for connecting MySqlDataBase Added @Entity annotations on Top of models Product and Category so that the Hibernate JDBC can create tables in the database on its own. Added @Id annotations on Top of Long id in product and category so that the Hibernate JDBC can treat them as unique ids. Addded @ManytoOne Relation on top of Cateogory so that HIbernate can define the cardinality of the relation between product and cateogry. --------------------------------------------------------xxxxxxxxxxx-------------------------------------------------------------------.

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,5 +1,8 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +13,14 @@ import java.util.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Product {
-
+    @Id
     private Long id;
     private String title;
     private String description;
     private Double price;
+    @ManyToOne
     private Category category;
     private String image;
 
